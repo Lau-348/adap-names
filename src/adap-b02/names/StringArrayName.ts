cad296710101b1ccb4fdf3e8799d8558c36f8866
lau-348 <laurin@praxis-schmid.com>
2024-10-29 15:45:47 +0100

    Finished Homework2, passing all tests

--- src/adap-b02/names/StringArrayName.ts
+++ src/adap-b02/names/StringArrayName.ts
@@ -6,51 +6,108 @@ export class StringArrayName implements Name {
     protected delimiter: string = DEFAULT_DELIMITER;
 
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation");
+        this.components = other;
+        if (delimiter != null && delimiter != undefined){
+            this.delimiter = delimiter;
+        }
+    }
+
+    protected doGetComponents(): string[] {
+        return this.components;
+    }
+
+    protected doSetComponent(i: number, other: string): void {
+        this.components[i] = other;
+    }
+
+    protected doSetAllComponents(other: string[]): void {
+        this.components = other;
+    }
+
+    protected doGetDelimiter(): string {
+        return this.delimiter;
+    }
+
+    public getComponents(): string[] {
+        return this.doGetComponents();
+    }
+
+    public setAllComponents(other: string[]): void {
+        this.doSetAllComponents(other);
+    }
+
+    public getDelimiter(): string {
+        return this.doGetDelimiter();
     }
 
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
+        return this.getComponents().join(delimiter);
     }
 
     public asDataString(): string {
-        throw new Error("needs implementation");
+        return this.asString().replace(this.getDelimiter(), ESCAPE_CHARACTER + this.delimiter);
     }
 
     public isEmpty(): boolean {
-        throw new Error("needs implementation");
+        return this.getComponents().length === 0;
     }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation");
+        return this.getDelimiter();
     }
 
     public getNoComponents(): number {
-        throw new Error("needs implementation");
+        return this.getComponents().length;
     }
 
     public getComponent(i: number): string {
-        throw new Error("needs implementation");
+        if (i <= this.getNoComponents()){
+            return this.getComponents()[i];
+        }
+        else {
+            throw new Error("Index out of range");
+        }
     }
 
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation");
+        if (i <= this.getNoComponents()){
+            this.doSetComponent(i, c);
+        }
+        else {
+            throw new Error("Index out of range");
+        }
     }
 
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation");
+        if (i <= this.getNoComponents()){
+            this.setAllComponents(this.getComponents().slice(0, i).concat([c], this.components.slice(i)));
+        }
+        else {
+            throw new Error("Index out of range");
+        }
     }
 
     public append(c: string): void {
-        throw new Error("needs implementation");
+        let components: string[] = this.getComponents();
+        components.push(c);
+        this.setAllComponents(components);
     }
 
     public remove(i: number): void {
-        throw new Error("needs implementation");
+        if (i <= this.getNoComponents()){
+            let components: string[] = this.getComponents();
+            components.splice(i, 1);
+            this.setAllComponents(components);
+        }
+        else {
+            throw new Error("Index out of range");
+        }
     }
 
     public concat(other: Name): void {
-        throw new Error("needs implementation");
+        let otherArray: string[] = other.asString(".").split(".");
+        let components: string [] = this.getComponents();
+        this.setAllComponents(components.concat(otherArray));
     }
 
 }
